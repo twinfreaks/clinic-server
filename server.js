4422d52b11df26241c694192b3f8a67c2921c909
Vitalii Yaremchuk <vitalyar7@gmail.com>
2017-05-27 18:45:13 +0300

    cors

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
 // }));
 
 app.use("/upload-photo", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://theclinic.herokuapp.com/");
+  res.header("Access-Control-Allow-Origin", "https://theclinic.herokuapp.com");
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
