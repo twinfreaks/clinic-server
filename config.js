c2eb0eabc496a8b0a073a1b0b892401dbfe8cdbc
Vitalii Yaremchuk <vitalyar7@gmail.com>
2017-05-27 17:50:18 +0300

    mongo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.db = "mongodb://localhost/clinic";
+config.db = "mongodb://heroku_lqtw5x5j:heroku_lqtw5x5j@ds151631.mlab.com:51631/heroku_lqtw5x5j";
 config.hashAlgorithm = "sha256";
 
 module.exports = config;
